80df1bf33696b67e7415f5243f0c0563ef4bcaa0
johna-3212 <tipst660@gmail.com>
2020-05-11 12:53:19 +0530

    Functions

--- Src/Bot/commands/moderation/ban.js
+++ Src/Bot/commands/moderation/ban.js
@@ -10,8 +10,13 @@ module.exports = class BanCommand extends Command {
     });
   }
 
-  async run(message, [name, ...args]) {
+  async run(message, [name, ...reason]) {
   if (!name) return message.sm("Please provide a member");
-  const target = message.
+  const target = message.findMember(name);
+  if (!target) return message.sm("Unable to find the specified member");
+  const r = reason.join(" ") || "Reason not provided";
+  const position = message.position(target);
+  if (position <= 0) return message.sm("Member has higher or equal role than you");
+
   }
 }
